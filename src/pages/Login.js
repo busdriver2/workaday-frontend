75b49cc907c00493488d2d9b9a17149627689518
busdriver2 <seonguks@uci.edu>
2024-09-30 14:06:53 -0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,23 +19,27 @@ const Login = () => {
         navigate('/signup')
     }
 
+    const toggleInfo = () => {
+        setShowInfo(!showInfo);
+    };
+
     return (
-        <div>
+        <div className="login-container">
             <form className="login" onSubmit={handleSubmit}>
                 <h3>Log in</h3>
 
                 <label>Email:</label>
                 <input 
-                type="text"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
+                    type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                 />
 
                 <label>Password:</label>
                 <input 
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                 />
 
                 <button disabled={isLoading}>Log in</button>
@@ -43,7 +47,32 @@ const Login = () => {
                 <button onClick={handleClick}>Sign up</button>
             </form>
 
-            
+            {/* Additional Content Section */}
+            <div className="app-info">
+                <button className="info-toggle" onClick={toggleInfo}>
+                    {showInfo ? 'Hide' : 'Learn More About the App'}
+                </button>
+                {showInfo && (
+                    <div className="info-content">
+                        <h4>Welcome to Our App</h4>
+                        <p>
+                            This app is designed to help you enhance your vocabulary through engaging
+                            and interactive tests. Once you log in, you'll be able to track your progress,
+                            select personalized learning packs, and more.
+                        </p>
+                        <p>
+                            Haven't signed up yet? Click the "Sign up" button to create your account and start learning today!
+                        </p>
+                        <h4>How it works:</h4>
+                        <ul>
+                            <li>Create an account or log in if you already have one.</li>
+                            <li>Take an initial test to assess your vocabulary level.</li>
+                            <li>Select a learning pack based on your level.</li>
+                            <li>Start learning and track your progress over time!</li>
+                        </ul>
+                    </div>
+                )}
+            </div>
         </div>
         
         
