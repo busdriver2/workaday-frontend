938a43585393693ca8859505205345b81090d232
busdriver2 <seonguks@uci.edu>
2024-11-06 15:52:52 -0800

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,6 +26,10 @@ const Login = () => {
 
     return (
         <div className="login-container">
+            <div>
+                <h2>Word a Day</h2>
+                <h3>Learn just one word a day. </h3>
+            </div>
             <form className="login" onSubmit={handleSubmit}>
                 <h3>Log in</h3>
 
@@ -48,32 +52,7 @@ const Login = () => {
                 <button onClick={handleClick}>Sign up</button>
             </form>
 
-            {/* Additional Content Section */}
-            <div className="app-info">
-                <button className="info-toggle" onClick={toggleInfo}>
-                    {showInfo ? 'Hide' : 'Learn More About the App'}
-                </button>
-                {showInfo && (
-                    <div className="info-content">
-                        <h4>Welcome to Our App</h4>
-                        <p>
-                            This app is designed to help you enhance your vocabulary through engaging
-                            and interactive tests. Once you log in, you'll be able to track your progress,
-                            select personalized learning packs, and more.
-                        </p>
-                        <p>
-                            Haven't signed up yet? Click the "Sign up" button to create your account and start learning today!
-                        </p>
-                        <h4>How it works:</h4>
-                        <ul>
-                            <li>Create an account or log in if you already have one.</li>
-                            <li>Take an initial test to assess your vocabulary level.</li>
-                            <li>Select a learning pack based on your level.</li>
-                            <li>Start learning and track your progress over time!</li>
-                        </ul>
-                    </div>
-                )}
-            </div>
+            
         </div>
         
         
